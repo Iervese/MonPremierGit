e79435d01712c54624adc85cf0cd97cffbb0e6e8
hagot <44870867+hagot@users.noreply.github.com>
2020-09-03 15:10:44 +0200

    modif de test

--- consoleSNimage/main.cpp
+++ consoleSNimage/main.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include "SNImage.h"
 using namespace std;
-main()
+main()//coucou
 {   SNImage img;
     img.Charger("a.bmp");
     cout<<img.Hauteur()<<"x"<<img.Largeur()<<endl;
